716e3126165988d40c99b42ef0bf4287330318a1
Yanfei Zhang <lastie@live.cn>
2015-02-16 17:15:16 -0500

    I've completed the UFO homework on my AW laptop.

---
+++ Assets/Move.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.A)) {
+			transform.position += new Vector3(0f, 0f, -70f) * Time.deltaTime;
+		} else if (Input.GetKey (KeyCode.D)) {
+			transform.position += new Vector3(0f, 0f, 70f) * Time.deltaTime;
+		} else if (Input.GetKey (KeyCode.W)) {
+			transform.position += new Vector3(-70f, 0f, 0f) * Time.deltaTime;
+		} else if (Input.GetKey (KeyCode.S)) {
+			transform.position += new Vector3(70f, 0f, 0f) * Time.deltaTime;
+		};
+	}
+}
---
+++ Assets/textDisplay.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class textDisplay : MonoBehaviour {
+
+	GameObject UFO;
+	GameObject treasure;
+	bool haveYouWon = false;
+
+	// Use this for initialization
+	void Start () {
+		UFO = GameObject.Find("UFO");
+		treasure = GameObject.Find ("treasureChest");
+	}
+	
+	// Update is called once per frame
+	void Update (){
+		float dist = Vector3.Distance(treasure.transform.position,(UFO.transform.position));
+		string textBuffer = "Your distance from the treasure is:" + dist.ToString();
+		if (!haveYouWon) {
+			if (dist < 260) {
+				textBuffer += "\nCongratulations! The treasure is directly below you. \nPress [SPACE] to obtain treasure.";
+				if (Input.GetKeyDown(KeyCode.Space)) {
+					haveYouWon = true;
+				}
+			} else if (dist < 700) {
+				textBuffer += "\nThe treasure is around you somewhere.";
+			} else if (dist < 1000) {
+				textBuffer += "\nKeep looking.";
+			} else {
+				textBuffer += "\nYou're quite far away from the treasure.";
+			}
+
+		} else {
+			textBuffer += "\nYou have won."; 
+		}
+		GetComponent<Text>().text = textBuffer;
+	}
+}
